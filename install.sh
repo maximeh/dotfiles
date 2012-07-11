35779025029f3ac62128347479a62d3a2fae4213
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2012-07-11 10:11:46 +0200

    Remove string operator as they are useless here, protect var by quotes. Also add a case to copy ssh config file.

--- install.sh
+++ install.sh
@@ -7,29 +7,33 @@ set -e
 GIT=$(which git)
 DOTFILES="$HOME/.dotfiles"
 
-[ -f ${GIT} ] || $(echo "Please install git -> apt-get install git" && exit 1)
+[ -f "$GIT" ] || $(echo "Please install git -> apt-get install git" && exit 1)
 
-if [ -d ${DOTFILES} ]; then
-    pushd "${DOTFILES}" > /dev/null
+if [ -d "$DOTFILES" ]; then
+    pushd "$DOTFILES" > /dev/null
     # Update them    
-    ${GIT} pull --recurse-submodules origin master
+    $GIT pull --recurse-submodules origin master
     popd > /dev/null
     exit 0
 fi
 
-${GIT} clone --recursive git://github.com/maximeh/dotfiles.git ${DOTFILES}
-pushd "${DOTFILES}" > /dev/null
+$GIT clone --recursive git://github.com/maximeh/dotfiles.git "$DOTFILES"
+pushd "$DOTFILES" > /dev/null
 
 # Create symlink
 for file in vimrc vim zshrc screenrc irssi; do
-    [ -h "${HOME}/.${file}" ] && rm ${HOME}/.${file}
-    ln -s ${DOTFILES}/${file} ${HOME}/.${file}
+    [ -h "$HOME/.$file" ] && rm "$HOME/.$file"
+    ln -s "$DOTFILES/$file" "$HOME/.$file"
 done
-[ -h "${HOME}/bin" ] && rm ${HOME}/bin
-ln -s ${DOTFILES}/bin ${HOME}/bin
+# Special case for SSH, don't want to symlink, just copy the config
+[ ! -d "$HOME/.ssh" ] && mkdir -p "$HOME/.ssh"
+cp "$DOTFILES/ssh/config" "$HOME/.ssh"
+
+[ -h "$HOME/bin" ] && rm "$HOME/bin"
+ln -s "$DOTFILES/bin" "$HOME/bin"
 
 # If we're on a Mac execute hack.sh
-[ "$(uname)" = "Darwin" ] && ${DOTFILES}/osx.sh
+[ "$(uname)" = "Darwin" ] && "$DOTFILES/osx.sh"
 
 popd > /dev/null
 exit 0
